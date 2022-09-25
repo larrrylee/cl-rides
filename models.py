cc7d1b4469bb525642c808924d917b68aa568581
Timothy <tzw202623@yahoo.com>
2022-09-24 21:50:49 -0700

    Implemented add rider to drivers

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import string
 
 
@@ -10,8 +11,18 @@ class Rider:
         self.driver = None
 
 class Driver:
-    def __init__(self, name: string, phone: string):
+    def __init__(self, name: string, phone: string, capacity: int):
         #TODO
         self.name = name
         self.phone = phone
+        self.capacity = capacity
         self.riders = []
+    
+
+    def add_rider(self, rider: Rider) -> bool:
+        if len(self.riders) < self.capacity and self.riders.count(rider) == 0:
+            rider.driver = self
+            self.riders.append(rider)
+            self.capacity += 1
+            return True
+        return False
